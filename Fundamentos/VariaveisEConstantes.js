168aa56965884ee9abe09f3664be88628c6fdb5c
[Jonas-eng-21] <92036392+Jonas-eng-21@users.noreply.github.com>
2022-04-04 21:00:18 -0300

    Estudando JS e React com TypeScript

---
+++ Fundamentos/VariaveisEConstantes.js
@@ -0,0 +1,9 @@
+var a = 3
+// var = palavra reservada para variáveis
+// Posso redeclarar essa variável algumas vezes
+// Os identificadores são mto importante, saiba atrinuir bons nomes
+let b = 4
+// let = palavra reservada tbm, mais indicada que 'var', tbm para variáveis
+
+const c = 5
+// const = constante
